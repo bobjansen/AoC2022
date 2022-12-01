1648e7e0b55dda98f1d4ee4d96c63a8f9fbf7da4
Bob Jansen <bobjansen@gmail.com>
2022-12-01 08:13:39 +0100

    Initial commit

---
+++ day1.R
@@ -0,0 +1,16 @@
+# Puzzle 1
+file <- "input1.txt"
+txt <- readChar(file, file.info(file)$size)
+blocks <- strsplit(txt, "\n\n")[[1L]]
+
+sum_calories <- function(block) {
+  lines <- strsplit(block, "\n")[[1L]]
+  sum(strtoi(lines, 10L))[[1L]]
+}
+names(calorie_counts) <- paste("elf", seq(length(calorie_counts)))
+
+calorie_counts <- sapply(blocks, sum_calories)
+cat("Puzzle 1:", max(calorie_counts), "\n")
+
+# Puzzle 2
+cat("Puzzle 2:", sum(-sort(-calorie_counts)[1:3]), "\n")
