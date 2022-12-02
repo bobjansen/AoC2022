8fa1116b0956c6ab37db0c259bca2281cee018a2
Bob Jansen <bobjansen@gmail.com>
2022-12-02 08:16:51 +0100

    Complete day 2 puzzles

---
+++ day2.R
@@ -0,0 +1,64 @@
+library(data.table)
+
+# Puzzle 3
+
+translateThem <- function(them) {
+  them[them == "A"] <- "Rock"
+  them[them == "B"] <- "Paper"
+  them[them == "C"] <- "Scissors"
+  them
+}
+
+translateMe <- function(me) {
+  me[me == "X"] <- "Rock"
+  me[me == "Y"] <- "Paper"
+  me[me == "Z"] <- "Scissors"
+  me
+}
+
+winner <- function(them , me) {
+  scores <- rep(0L, length(them)) # Initialize to loss
+  scores[them == me] <- 3L
+  scores[them == "Rock" & me == "Paper"] <- 6L
+  scores[them == "Paper" & me == "Scissors"] <- 6L
+  scores[them == "Scissors" & me == "Rock"] <- 6L
+  scores
+}
+
+dt <- data.table::fread('input2.csv', header = FALSE, sep = ' ')
+names(dt) <- c("Them", "Me")
+dt[, ':='(Them = translateThem(Them), Me = translateMe(Me))]
+
+calculateScore <- function(dt) {
+  dt[, ScoreResult := winner(Them, Me)]
+
+  dt[, ScoreChoice := 1L]
+  dt[Me == "Paper", ScoreChoice := 2L]
+  dt[Me == "Scissors", ScoreChoice := 3L]
+
+  dt[, Score := ScoreResult + ScoreChoice]
+  dt[, sum(Score)]
+}
+cat("My score:", calculateScore(dt), "\n")
+
+# Puzzle 4
+
+choices <- c("Rock", "Paper", "Scissors")
+losesTo <- c("Scissors", "Rock", "Paper")
+winsFrom <- c("Paper", "Scissors", "Rock")
+
+fixResultColumn <- function(them, result) {
+  selX <- result == "Rock"
+  selY <- result == "Paper"
+  selZ <- result == "Scissors"
+
+  me <- rep("", length(them))
+  me[selX] <- losesTo[match(them[selX], choices)]
+  me[selY] <- them[selY]
+  me[selZ] <- winsFrom[match(them[selZ], choices)]
+  me
+}
+
+dt[, Me := fixResultColumn(Them, Me)]
+
+cat("My score:", calculateScore(dt), "\n")
