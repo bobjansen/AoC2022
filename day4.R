6a7e0bd0f8ea15f4b92f75b81fcbe07dbfc0ffa7
Bob Jansen <bobjansen@gmail.com>
2022-12-04 13:25:46 +0100

    Use .SDcols and Qualify call to foverlaps()

--- day4.R
+++ day4.R
@@ -3,8 +3,9 @@ library(data.table)
 assignments <- data.table::fread("input4.csv", sep=",", header = FALSE)
 assignments[, c("lb1", "ub1") := tstrsplit(V1, "-")]
 assignments[, c("lb2", "ub2") := tstrsplit(V2, "-")]
-assignments <- assignments[, .(lb1, ub1, lb2, ub2)]
-assignments <- assignments[, lapply(.SD, as.integer)]
+assignments <- assignments[,
+  lapply(.SD, as.integer), .SDcols = c("lb1", "ub1", "lb2", "ub2")
+]
 
 contained <- assignments[(lb1 >= lb2 & ub1 <= ub2) | (lb2 >= lb1 & ub2 <= ub1)]
 cat("Puzzle 7:", nrow(contained), "\n")
@@ -15,5 +16,5 @@ right <- assignments[, .(ID = .I, lb2, ub2)]
 data.table::setkeyv(left, c("ID", "lb1", "ub1"))
 data.table::setkeyv(right, c("ID", "lb2", "ub2"))
 
-overlapped <- foverlaps(left, right)[!is.na(lb2)]
+overlapped <- data.table::foverlaps(left, right)[!is.na(lb2)]
 cat("Puzzle 8:", nrow(overlapped), "\n")
