f84cc27ae9c84327cea2895f7430e69e1a98895e
Bob Jansen <bobjansen@gmail.com>
2022-12-04 13:25:12 +0100

    Solve day 4

---
+++ day4.R
@@ -0,0 +1,19 @@
+library(data.table)
+
+assignments <- data.table::fread("input4.csv", sep=",", header = FALSE)
+assignments[, c("lb1", "ub1") := tstrsplit(V1, "-")]
+assignments[, c("lb2", "ub2") := tstrsplit(V2, "-")]
+assignments <- assignments[, .(lb1, ub1, lb2, ub2)]
+assignments <- assignments[, lapply(.SD, as.integer)]
+
+contained <- assignments[(lb1 >= lb2 & ub1 <= ub2) | (lb2 >= lb1 & ub2 <= ub1)]
+cat("Puzzle 7:", nrow(contained), "\n")
+
+left <- assignments[, .(ID = .I, lb1,  ub1)]
+right <- assignments[, .(ID = .I, lb2, ub2)]
+
+data.table::setkeyv(left, c("ID", "lb1", "ub1"))
+data.table::setkeyv(right, c("ID", "lb2", "ub2"))
+
+overlapped <- foverlaps(left, right)[!is.na(lb2)]
+cat("Puzzle 8:", nrow(overlapped), "\n")
