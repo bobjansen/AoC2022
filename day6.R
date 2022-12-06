f96458f2ce5c2c6ebeab519595d64df9f4288d1e
Bob Jansen <bobjansen@gmail.com>
2022-12-06 08:15:47 +0100

    Solutions for day 6

---
+++ day6.R
@@ -0,0 +1,38 @@
+source("helpers.R")
+
+streams <- c(
+  "mjqjpqmgbljsphdztnvjfqwrcgsmlb",
+  "bvwbjplbgvbhsrlpgdmjqwftvncz",
+  "nppdvjthqldpwncqszvftbrmjlhg",
+  "nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg",
+  "zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw"
+)
+
+find_marker <- function(stream, marker_size) {
+  chars <- ustrsplit(stream, "")
+  buffer <- c()
+  for (i in seq_along(chars)) {
+    char <- chars[[i]]
+    index <- match(char, buffer)
+    buffer <- c(buffer, char)
+    if (!is.na(index)) {
+      buffer <- buffer[(index + 1L):length(buffer)]
+    }
+    if (length(buffer) == marker_size) {
+      catn(paste0(buffer, collapse = ""))
+      return(i)
+    }
+  }
+  invisible()
+}
+
+# Puzzle 11
+sapply(streams, find_marker, marker_size = 4L)
+cat("\n")
+cat_solution(11L, find_marker(readFile("input6.txt"), marker_size = 4L))
+cat("\n")
+
+# Puzzle 12
+sapply(streams, find_marker, marker_size = 14L)
+cat("\n")
+cat_solution(12L, find_marker(readFile("input6.txt"), marker_size = 14L))
