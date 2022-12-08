e3c0cc5481236c789a4cc43819606777ea8bf263
Bob Jansen <bobjansen@gmail.com>
2022-12-08 21:02:01 +0100

    Small speed up

--- day8.R
+++ day8.R
@@ -1,7 +1,6 @@
 source("helpers.R")
 
 txt <- readFile("input8.txt")
-
 rows <- ustrsplit(txt, "\n")
 num_rows <- length(rows)
 matrix <- strsplit(rows, "")
@@ -28,14 +27,7 @@ for (tree_index in forest[on_edge == FALSE, ID]) {
   the_column <- forest[tree_index, column]
   the_tree <- forest[tree_index, tree]
 
-  if (forest[row == the_row & column < the_column, any(tree >= the_tree)] &&
-      forest[row == the_row & column > the_column, any(tree >= the_tree)] &&
-      forest[row < the_row & column == the_column, any(tree >= the_tree)] &&
-      forest[row > the_row & column == the_column, any(tree >= the_tree)]) {
-    forest[tree_index, visible := FALSE]
-  }
 }
-cat_solution(15, forest[, sum(visible)])
 
 find_blocker <- function(vec) {
   if (any(vec)) {
@@ -45,21 +37,30 @@ find_blocker <- function(vec) {
   }
 }
 
+data.table::setkeyv(forest, c("row", "column"))
+
 max_scenic_score <- 0L
 for (tree_index in forest[on_edge == FALSE, ID]) {
   the_row <- forest[tree_index, row]
   the_column <- forest[tree_index, column]
   the_tree <- forest[tree_index, tree]
 
-  left <- find_blocker(
-    forest[row == the_row & column < the_column, rev(tree)] >= the_tree)
-  right <- find_blocker(
-    forest[row == the_row & column > the_column, tree] >= the_tree)
-  top <- find_blocker(
-    forest[row < the_row & column == the_column, rev(tree)] >= the_tree)
-  bottom <- find_blocker(
-    forest[row > the_row & column == the_column, tree] >= the_tree)
+  left_trees <- forest[row == the_row & column < the_column, tree]
+  right_trees <- forest[row == the_row & column > the_column, tree]
+  top_trees <- forest[row < the_row & column == the_column, tree]
+  bottom_trees <- forest[row > the_row & column == the_column, tree]
+
+  if (any(left_trees >= the_tree) && any(right_trees >= the_tree) &&
+      any(top_trees >= the_tree) && any(bottom_trees >= the_tree)) {
+    forest[tree_index, visible := FALSE]
+  }
+
+  left <- find_blocker(rev(left_trees) >= the_tree)
+  right <- find_blocker(right_trees >= the_tree)
+  top <- find_blocker(rev(top_trees) >= the_tree)
+  bottom <- find_blocker(bottom_trees >= the_tree)
   scenic_score <- left * right * top * bottom
   max_scenic_score = max(scenic_score, max_scenic_score)
 }
+cat_solution(15, forest[, sum(visible)])
 cat_solution(16, max_scenic_score)
