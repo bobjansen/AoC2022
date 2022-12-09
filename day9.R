24a0f48c799cfd9c868f009125ad7febe4211635
Bob Jansen <bobjansen@gmail.com>
2022-12-09 23:34:59 +0100

    Optimized day 9

--- day9.R
+++ day9.R
@@ -1,79 +1,38 @@
 source("helpers.R")
 
+# Enriched with the ideas from:
+# https://www.reddit.com/r/adventofcode/comments/zgnice/comment/izhzxb6/
+
 txt <- readFile("input9.txt")
 moves <- strsplit(ustrsplit(txt, "\n"), " ")
 
 directions <- list(
-  R = complex(1, 1, 0),
-  D = complex(1, 0, -1),
-  L = complex(1, -1, 0),
-  U = complex(1, 0, 1)
+  R = c(1L, 0L),
+  D = c(0L, -1L),
+  L = c(-1L, 0L),
+  U = c(0L, 1L)
 )
 
-calculate_change <- function(p1, p2, last_move) {
-  move <- complex(1, 0, 0)
-
-  delta <- p1 - p2
-  manhattan_distance <- abs(Re(delta)) + abs(Im(delta))
-
-  if (manhattan_distance == 4) {
-    move <- last_move
-  } else if (manhattan_distance == 3) {
-    if (Re(delta) == 2) {
-      move <- move + directions[["R"]]
-    } else if (Re(delta) == -2) {
-      move <- move + directions[["L"]]
-    } else if (Im(delta) == 2) {
-      move <- move + directions[["U"]]
-    } else if (Im(delta) == -2) {
-      move <- move + directions[["D"]]
-    }
-    if (Re(delta) == 1) {
-      move <- move + directions[["R"]]
-    } else if (Re(delta) == -1) {
-      move <- move + directions[["L"]]
-    } else if (Im(delta) == 1) {
-      move <- move + directions[["U"]]
-    } else if (Im(delta) == -1) {
-      move <- move + directions[["D"]]
-    }
-  } else if (manhattan_distance == 2) {
-    if (Re(delta) == 2) {
-      move <- move + directions[["R"]]
-    } else if (Re(delta) == -2) {
-      move <- move + directions[["L"]]
-    } else if (Im(delta) == 2) {
-      move <- move + directions[["U"]]
-    } else if (Im(delta) == -2) {
-      move <- move + directions[["D"]]
-    }
-  }
-  move
-}
-
 do_moves <- function(lines, snake_size = 2L) {
-  snake <- replicate(snake_size, complex(1, 0, 0))
-  tail_path <- c()
+  snake <- matrix(0L, 10L, 2L)
+  tail_path_1 <- dictionary()
+  tail_path_9 <- dictionary()
 
   for (line in lines) {
-    direction <- directions[[line[[1]]]]
-    move_length <- line[[2L]]
-
-    for (i in seq_len(move_length)) {
-      snake[[1L]] <- snake[[1L]] + direction
-      last_move <- direction
+    for (i in 1:line[[2L]]) {
+      snake[1L,] <- snake[1L,] + directions[[line[[1L]]]]
 
       for (j in 2:snake_size) {
-        last_move <- calculate_change(snake[[j - 1L]], snake[[j]], last_move)
-        snake[[j]] <- snake[[j]] + last_move
+        delta <- snake[j - 1L,] - snake[j,]
+        if (abs(delta[[1L]]) >= 2L || abs(delta[[2L]]) >= 2L) {
+          snake[j, ] <- snake[j, ] + c(sign(delta[[1L]]), sign(delta[[2L]]))
+        }
       }
-      tail_path <- c(tail_path, tail(snake, 1L))
+      tail_path_1 <- setValue(tail_path_1, toString(snake[2L,]), TRUE)
+      tail_path_9 <- setValue(tail_path_9, toString(snake[10L,]), TRUE)
     }
   }
-  tail_path
+  cat_solution(17, size(tail_path_1))
+  cat_solution(18, size(tail_path_9))
 }
-
-tail_path <- do_moves(moves)
-cat_solution(17, length(unique(tail_path)))
-tail_path <- do_moves(moves, 10L)
-cat_solution(18, length(unique(tail_path)))
+do_moves(moves, 10L)
