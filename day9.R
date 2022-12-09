550f80256229631895b354874c3d6f48c8162a85
Bob Jansen <bobjansen@gmail.com>
2022-12-09 17:41:35 +0100

    Refactor

--- day9.R
+++ day9.R
@@ -9,12 +9,9 @@ D 10
 L 25
 U 20"
 
-# txt   <- readFile("input9.txt")
+txt   <- readFile("input9.txt")
 moves <- strsplit(ustrsplit(txt, "\n"), " ")
 moves
-
-pos_h <- complex(1, 0, 0)
-pos_t <- complex(1, 0, 0)
 
 directions <- list(
   R = complex(1, 1, 0),
@@ -23,48 +20,60 @@ directions <- list(
   U = complex(1, 0, 1)
 )
 
-tail_path <- c()
-
-do_move <- function(direction_letter, move_length) {
-  direction <- directions[[direction_letter]]
-
-  for (i in seq_len(move_length)) {
-    pos_h <<- pos_h + direction
-    delta <- pos_h - pos_t
-    manhattan_distance <- abs(Re(delta)) + abs(Im(delta))
-    if (manhattan_distance == 3) {
-      if (Re(delta) == 2) {
-        pos_t <<- pos_t + directions[["R"]]
-      } else if (Re(delta) == -2) {
-        pos_t <<- pos_t + directions[["L"]]
-      } else if (Im(delta) == 2) {
-        pos_t <<- pos_t + directions[["U"]]
-      } else if (Im(delta) == -2) {
-        pos_t <<- pos_t + directions[["D"]]
-      }
-      if (Re(delta) == 1) {
-        pos_t <<- pos_t + directions[["R"]]
-      } else if (Re(delta) == -1) {
-        pos_t <<- pos_t + directions[["L"]]
-      } else if (Im(delta) == 1) {
-        pos_t <<- pos_t + directions[["U"]]
-      } else if (Im(delta) == -1) {
-        pos_t <<- pos_t + directions[["D"]]
-      }
-    } else if (manhattan_distance == 2) {
-      if (Re(delta) == 2) {
-        pos_t <<- pos_t + directions[["R"]]
-      } else if (Re(delta) == -2) {
-        pos_t <<- pos_t + directions[["L"]]
-      } else if (Im(delta) == 2) {
-        pos_t <<- pos_t + directions[["U"]]
-      } else if (Im(delta) == -2) {
-        pos_t <<- pos_t + directions[["D"]]
-      }
+calculate_change <- function(p1, p2) {
+  delta <- p1 - p2
+  manhattan_distance <- abs(Re(delta)) + abs(Im(delta))
+  if (manhattan_distance == 3) {
+    if (Re(delta) == 2) {
+      p2 <- p2 + directions[["R"]]
+    } else if (Re(delta) == -2) {
+      p2 <- p2 + directions[["L"]]
+    } else if (Im(delta) == 2) {
+      p2 <- p2 + directions[["U"]]
+    } else if (Im(delta) == -2) {
+      p2 <- p2 + directions[["D"]]
     }
-    tail_path <<- c(tail_path, pos_t)
+    if (Re(delta) == 1) {
+      p2 <- p2 + directions[["R"]]
+    } else if (Re(delta) == -1) {
+      p2 <- p2 + directions[["L"]]
+    } else if (Im(delta) == 1) {
+      p2 <- p2 + directions[["U"]]
+    } else if (Im(delta) == -1) {
+      p2 <- p2 + directions[["D"]]
+    }
+  } else if (manhattan_distance == 2) {
+    if (Re(delta) == 2) {
+      p2 <- p2 + directions[["R"]]
+    } else if (Re(delta) == -2) {
+      p2 <- p2 + directions[["L"]]
+    } else if (Im(delta) == 2) {
+      p2 <- p2 + directions[["U"]]
+    } else if (Im(delta) == -2) {
+      p2 <- p2 + directions[["D"]]
+    }
   }
+  p2
 }
 
-sapply(moves, \(line) do_move(line[[1]], line[[2L]]))
+do_moves <- function(lines) {
+  pos_h <- complex(1, 0, 0)
+  pos_t <- complex(1, 0, 0)
+  tail_path <- c()
+
+  for (line in lines) {
+    direction_letter <- line[[1]]
+    move_length <- line[[2L]]
+    direction <- directions[[direction_letter]]
+
+    for (i in seq_len(move_length)) {
+      pos_h <- pos_h + direction
+      pos_t <- calculate_change(pos_h, pos_t)
+      tail_path <- c(tail_path, pos_t)
+    }
+  }
+  tail_path
+}
+
+tail_path <- do_moves(moves)
 cat_solution(17, length(unique(tail_path)))
