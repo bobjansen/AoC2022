6f4ddeedd98105fe1cca15701b7ae0052d2be685
Bob Jansen <bobjansen@gmail.com>
2022-12-09 18:16:41 +0100

    Finish part 2

--- day9.R
+++ day9.R
@@ -20,56 +20,62 @@ directions <- list(
   U = complex(1, 0, 1)
 )
 
-calculate_change <- function(p1, p2) {
+calculate_change <- function(p1, p2, last_move) {
   delta <- p1 - p2
+  move <- complex(1, 0, 0)
   manhattan_distance <- abs(Re(delta)) + abs(Im(delta))
-  if (manhattan_distance == 3) {
+  if (manhattan_distance == 4) {
+    move <- last_move
+  } else if (manhattan_distance == 3) {
     if (Re(delta) == 2) {
-      p2 <- p2 + directions[["R"]]
+      move <- move + directions[["R"]]
     } else if (Re(delta) == -2) {
-      p2 <- p2 + directions[["L"]]
+      move <- move + directions[["L"]]
     } else if (Im(delta) == 2) {
-      p2 <- p2 + directions[["U"]]
+      move <- move + directions[["U"]]
     } else if (Im(delta) == -2) {
-      p2 <- p2 + directions[["D"]]
+      move <- move + directions[["D"]]
     }
     if (Re(delta) == 1) {
-      p2 <- p2 + directions[["R"]]
+      move <- move + directions[["R"]]
     } else if (Re(delta) == -1) {
-      p2 <- p2 + directions[["L"]]
+      move <- move + directions[["L"]]
     } else if (Im(delta) == 1) {
-      p2 <- p2 + directions[["U"]]
+      move <- move + directions[["U"]]
     } else if (Im(delta) == -1) {
-      p2 <- p2 + directions[["D"]]
+      move <- move + directions[["D"]]
     }
   } else if (manhattan_distance == 2) {
     if (Re(delta) == 2) {
-      p2 <- p2 + directions[["R"]]
+      move <- move + directions[["R"]]
     } else if (Re(delta) == -2) {
-      p2 <- p2 + directions[["L"]]
+      move <- move + directions[["L"]]
     } else if (Im(delta) == 2) {
-      p2 <- p2 + directions[["U"]]
+      move <- move + directions[["U"]]
     } else if (Im(delta) == -2) {
-      p2 <- p2 + directions[["D"]]
+      move <- move + directions[["D"]]
     }
   }
-  p2
+  move
 }
 
-do_moves <- function(lines) {
-  pos_h <- complex(1, 0, 0)
-  pos_t <- complex(1, 0, 0)
+do_moves <- function(lines, snake_size = 2L) {
+  snake <- replicate(snake_size, complex(1, 0, 0))
   tail_path <- c()
 
   for (line in lines) {
-    direction_letter <- line[[1]]
+    direction <- directions[[line[[1]]]]
     move_length <- line[[2L]]
-    direction <- directions[[direction_letter]]
 
     for (i in seq_len(move_length)) {
-      pos_h <- pos_h + direction
-      pos_t <- calculate_change(pos_h, pos_t)
-      tail_path <- c(tail_path, pos_t)
+      snake[[1L]] <- snake[[1L]] + direction
+      last_move <- direction
+
+      for (j in 2:snake_size) {
+        last_move <- calculate_change(snake[[j - 1L]], snake[[j]], last_move)
+        snake[[j]] <- snake[[j]] + last_move
+      }
+      tail_path <- c(tail_path, tail(snake, 1L))
     }
   }
   tail_path
@@ -77,3 +83,5 @@ do_moves <- function(lines) {
 
 tail_path <- do_moves(moves)
 cat_solution(17, length(unique(tail_path)))
+tail_path <- do_moves(moves, 10L)
+cat_solution(18, length(unique(tail_path)))
