2ce1ddc9cd6b3cc440535d59253d3b2a1e78aa80
Bob Jansen <bobjansen@gmail.com>
2022-12-12 23:05:09 +0100

    Optimze to less than half a second

--- day12.R
+++ day12.R
@@ -10,7 +10,7 @@ translate <- c("S", letters, "E")
 heights <- strsplit(ustrsplit(readFile("input12.txt"), "\n"), "")
 # heights <- strsplit(ustrsplit(txt, "\n"), "")
 heights <- sapply(heights, \(.) match(., translate) -1L)
-
+max_distance <- nrow(heights) * ncol(heights)
 directions <- list(c(1L, 0L), c(0L, -1L), c(-1L, 0L), c(0L, 1L))
 
 point_in_bounds <- function(point) {
@@ -18,67 +18,56 @@ point_in_bounds <- function(point) {
     point[[1L]] <= nrow(heights) && point[[2L]] <= ncol(heights)
 }
 
-find_coords <- function(index) {
-  row <- index %% nrow(heights)
-  column <- index %/% nrow(heights) + 1L
-  c(row, column)
-}
-
-update_shortest_path <- function(visited, point, distance) {
-  key <- toString(point)
-  current_distance <- visited[key]
-  is_new <- is.null(current_distance)
-  if (is_new || distance < current_distance) {
-    visited[key] <- distance
-  }
-  is_new
-}
-
-start_at <- find_coords(which(heights == 0L))
-end_at <- find_coords(which(heights == 27L))
-heights[start_at[[1L]], start_at[[2L]]] <- 0L
+start_at <- which(heights == 0L, arr.ind = TRUE)
+end_at <- which(heights == 27L, , arr.ind = TRUE)
+heights[start_at[[1L]], start_at[[2L]]] <- 1L
 heights[end_at[[1L]], end_at[[2L]]] <- 26L
-catn("Starts at: ", start_at[[1L]], ", ", start_at[[2L]], sep="")
-catn("Ends at: ", end_at[[1L]], ", ", end_at[[2L]], sep="")
 
 add_destinations <- function(source, visited, destinations) {
   current_height <- heights[source[[1L]], source[[2L]]]
-  distance_to_source <- visited[toString(source)]
+  distance_to_source <- visited[source[[1L]], source[[2L]]]
   for (direction in directions) {
     destination <- source + direction
     if (point_in_bounds(destination)) {
       destination_height <- heights[destination[[1L]], destination[[2L]]]
       if (destination_height >= current_height - 1L) {
-        is_new <- update_shortest_path(
-          visited, destination, distance_to_source + 1L
-        )
-        if (is_new) destinations <- append(destinations, list(destination))
+        old_distance <- visited[destination[[1L]], destination[[2L]]]
+        if (distance_to_source + 1L < old_distance) {
+          visited[destination[[1L]], destination[[2L]]] <-
+            distance_to_source + 1L
+          destinations <- c(destinations, list(destination))
+        }
       }
     }
   }
-  destinations
+  list(destinations = destinations, visited = visited)
 }
 
-visited <- dictionary()
-destinations <- list()
+bfs <- function() {
+  visited <- matrix(max_distance, nrow(heights), ncol(heights))
+  destinations <- list(end_at)
+  visited[end_at[[1L]], end_at[[2L]]] <- 0L
 
-update_shortest_path(visited, end_at, 0L)
-destinations <- add_destinations(end_at, visited, destinations)
-
-while (length(destinations) > 0L) {
-  test_destination <- destinations[[1L]]
-  destinations <- if (length(destinations) >= 2L) {
-    destinations[2:length(destinations)]
-  } else {
-    list()
+  repeat {
+    test_destination <- destinations[[1L]]
+    lst <- add_destinations(
+      test_destination, visited,
+      destinations[seq_len(length(destinations) - 1L) + 1L]
+    )
+    destinations <- lst[["destinations"]]
+    visited <- lst[["visited"]]
+    if (length(destinations) == 0L) break
   }
-  destinations <- add_destinations(test_destination, visited, destinations)
+  visited
 }
 
-print(visited[toString(start_at)])
+visited <- bfs()
 
-distances <- sapply(which(heights == 1L), function(index) {
-  point <- find_coords(index)
-  visited[toString(point)]
+cat_solution(23L, visited[start_at[[1L]], start_at[[2L]]])
+
+as <- which(heights == 1L, arr.ind = TRUE)
+distances <- sapply(1:nrow(as), function(i) {
+  point <- as[i,]
+  visited[point[[1L]], point[[2L]]]
 })
 cat_solution(24L, min(unlist(distances)))
