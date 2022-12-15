a53c89026270cfc7762ebf4df3a38c6c1df08895
Bob Jansen <bobjansen@gmail.com>
2022-12-15 21:04:48 +0100

    Hacked together part 2

--- day15.R
+++ day15.R
@@ -16,9 +16,11 @@ Sensor at x=14, y=3: closest beacon is at x=15, y=3
 Sensor at x=20, y=1: closest beacon is at x=15, y=3"
 
 target_row <- 10L
+upper_bound <- 20L
 
 txt <- readFile("input15.txt")
 target_row <- 2000000L
+upper_bound <- 4000000L
 
 extract_numbers <- function(line) {
   matches <- regmatches(line, gregexpr("-?[[:digit:]]+", line))
@@ -28,29 +30,70 @@ extract_numbers <- function(line) {
 sensors <- ustrsplit(txt, "\n")
 positions <- lapply(sensors, extract_numbers)
 
-
-overlap <- c()
-
-beacon_hits <- dictionary()
-
-for (position in positions) {
-  md <- abs(position[[1L]] - position[[3L]]) +
-    abs(position[[2L]] - position[[4L]])
-
-  y_dist <- abs(position[[2L]] - target_row)
-  delta <- md - y_dist
-  # browser()
-  if (delta > 0L) {
-    if (position[[4L]] == target_row) {
-      setValue(beacon_hits, toString(c(position[[3L]], position[[4L]])), TRUE)
+beacons <- list()
+for (pos in positions) {
+  x <- pos[[3L]]
+  y <- pos[[4L]]
+  if (x >= 0 && x <= upper_bound && y >= 0 && y <= upper_bound) {
+    x <- toString(x)
+    y <- toString(y)
+    if (!y %in% names(beacons)) {
+      beacons[[y]] <- list()
     }
-
-    catn("md:", md)
-    catn("y_dist:", y_dist)
-    catn("delta:", delta)
-
-    overlap <- c(overlap, (position[[1L]] - delta):(position[[1L]] + delta))
+    beacons[[y]][[toString(x)]] <- TRUE
   }
 }
 
-print(length(unique(overlap)) - size(beacon_hits))
+join_intervals <- function(lefts, rights) {
+  n_intervals <- length(lefts)
+  the_order <- order(lefts)
+  lefts <- lefts[the_order]
+  rights <- rights[the_order]
+  if (lefts[[1L]] > 0L || max(rights) < target_row) return(TRUE)
+
+  right_bound <- rights[[1L]]
+
+  for (i in seq_len(n_intervals - 1L) + 1L) {
+    if (lefts[[i]] > right_bound) return(TRUE)
+    right_bound <- max(right_bound, rights[[i]])
+  }
+  FALSE
+}
+
+scan_row <- function(positions, target_row) {
+  overlap <- c()
+
+  lefts <- c()
+  rights <- c()
+
+  for (pos in positions) {
+    md <- abs(pos[[1L]] - pos[[3L]]) + abs(pos[[2L]] - pos[[4L]])
+
+    y_dist <- abs(pos[[2L]] - target_row)
+    delta <- md - y_dist
+
+    if (delta > 0L) {
+      # catn("md:", md)
+      # catn("y_dist:", y_dist)
+      # catn("delta:", delta)
+      # overlap <- c(overlap, (pos[[1L]] - delta):(pos[[1L]] + delta))
+      lefts <- c(lefts, pos[[1L]] - delta)
+      rights <- c(rights, pos[[1L]] + delta)
+    }
+  }
+  # if (target_row == 11) browser()
+  join_intervals(lefts, rights)
+}
+
+print(length(unique(scan_row(positions, 10L))) -
+        length(beacons[[toString(target_row)]]))
+
+done <- FALSE
+i <- 0L
+while (!done && i < upper_bound) {
+  i <- i + 1L
+  if (i %% 100000L == 0L) catn("Row:", i)
+  done <- scan_row(positions, i)
+}
+catn("Row: ", i)
+
