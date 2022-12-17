394a39857bcb1cbf2efbb14f27e80764b4180cd1
Bob Jansen <bobjansen@gmail.com>
2022-12-17 19:58:40 +0100

    Slower but more correct

--- day16.R
+++ day16.R
@@ -25,14 +25,14 @@ setClass("Valve", representation(
 
 valve_ids <- c()
 candidates <- 0L
+potential_flow <- 0L
 create_valve <- function(line) {
   id <- line[[2L]]
   flow <- substr(line[[5L]], 6, nchar(line[[5L]]) - 1L)
   destinations <- gsub(",", "", line[10L:length(line)])
   flow <- as.integer(flow)
 
-  candidates <<- candidates + (flow > 0L)
-
+  potential_flow <<- potential_flow + flow
   valve_ids <<- c(valve_ids, id)
 
   new(
@@ -51,60 +51,50 @@ max_flow <- 0L
 location <- "AA"
 total_time <- 30L
 
-steps <- list()
-
-set.seed(42L)
-
 explore <- function(
-    valves, location, budget, current_flow, open_valves, flow_per_time, steps
+    valves, location, budget, current_flow, flow_per_time, last_location
 ) {
-  if (open_valves == candidates ||
-      current_flow + budget * (budget - 1L) * 11L < max_flow) {
+  if (current_flow + (budget - 1L) * (potential_flow - flow_per_time) <= max_flow) {
     return(current_flow)
   }
 
-  steps[[length(steps) + 1L]] <- list(
-    location = location,
-    flow_per_time = flow_per_time,
-    open_valves = open_valves
-  )
-
-
-  if (!valves[[location]]@open && valves[[location]]@flow > 0L) {
+  flow <- valves[[location]]@flow
+  destinations <- valves[[location]]@destinations
+  if (flow > 0L && !valves[[location]]@open) {
     valves[[location]]@open <- TRUE
 
-    new_flow <- current_flow + valves[[location]]@flow * (budget - 1L)
+    new_flow <- current_flow + flow * (budget - 1L)
     if (new_flow > max_flow) {
       max_flow <<- new_flow
       catn("Max flow:", max_flow)
     }
+    if (potential_flow - flow_per_time == 0L) {
+      return(current_flow)
+    }
     if (budget > 2L) {
-      for (destination in valves[[location]]@destinations) {
+      for (destination in destinations) {
         explore(
           valves,
           destination,
           budget - 2L,
           new_flow,
-          open_valves + 1L,
-          flow_per_time + valves[[location]]@flow,
-          steps)
+          flow_per_time + flow,
+          location)
       }
     }
   }
 
-  if (budget > 2L) {
-    for (destination in valves[[location]]@destinations) {
-      if (length(steps) == 1L ||
-          destination != steps[[length(steps) - 1L]]$location) {
+  if (current_flow +
+      (budget - 2L) * (potential_flow - flow_per_time) > max_flow) {
+    for (destination in destinations) {
+      if (destination != last_location) {
         explore(
           valves,
-          destination, budget - 1L, current_flow, open_valves, flow_per_time, steps)
+          destination, budget - 1L, current_flow, flow_per_time, location)
       }
     }
   }
-
-
 }
 
-explore(valves, "AA", total_time, 0L, 0L, 0L, steps)
+profvis::profvis(explore(valves, "AA", total_time, 0L, 0L, ""))
 cat_solution(31L, max_flow)
