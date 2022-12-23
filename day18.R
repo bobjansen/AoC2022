4844070da8eeed3fc70a99c97f9a352983f4ac93
Bob Jansen <bobjansen@gmail.com>
2022-12-23 20:02:45 +0100

    Add part 2

--- day18.R
+++ day18.R
@@ -33,9 +33,15 @@ neighbors <- c(
 )
 neighbors <- c(neighbors, -neighbors)
 
-cubes[, index := x * neighbors[[1L]] + y * neighbors[[2L]] + z + 1L]
+to_index <- function(x, y, z) {
+  x * neighbors[[1L]] + y * neighbors[[2L]] + z + 1L
+}
 
-space <- rep(FALSE, max_x * neighbors[[1L]] + max_y * neighbors[[2L]] + max_z)
+cubes[, index := to_index(x, y, z)]
+
+space_size <- max_x * neighbors[[1L]] + max_y * neighbors[[2L]] + max_z
+
+space <- rep(FALSE, space_size)
 for (index in cubes$index) {
   space[index] <- TRUE
 }
@@ -44,10 +50,44 @@ neighbor_count <- 0L
 for (index in cubes$index) {
   for (neighbor in neighbors) {
     candidate <- index + neighbor
-    if (candidate >= 0L && candidate <= length(space) && space[candidate]) {
+    if (candidate >= 1L && candidate <= length(space) && space[candidate]) {
       neighbor_count <- neighbor_count + 1L
     }
   }
 }
 
-cat_solution(35L, nrow(cubes) * 6L - neighbor_count)
+answer1 <- nrow(cubes) * 6L - neighbor_count
+cat_solution(35L, answer1)
+
+todo <- priorityQueue(to_index(0L, 0L, 0L))
+known <- rep(FALSE, space_size)
+visited <- rep(FALSE, space_size)
+hit <- rep(FALSE, space_size)
+
+while (!empty(todo)) {
+  index <- topAndPop(todo)
+  visited[[index]] <- TRUE
+  for (neighbor in neighbors) {
+    candidate <- index + neighbor
+    if (candidate >= 1L && candidate <= length(space) && !hit[[candidate]]) {
+      if (space[candidate]) {
+        hit[[index]] <- TRUE
+      } else if (!known[[candidate]] && !visited[[candidate]]) {
+        known[[candidate]] <- TRUE
+        push(todo, candidate)
+      }
+    }
+  }
+}
+
+neighbor_count <- 0L
+for (index in which(!space & !visited)) {
+  for (neighbor in neighbors) {
+    candidate <- index + neighbor
+    if (candidate >= 1L && candidate <= length(space) && space[candidate]) {
+      neighbor_count <- neighbor_count + 1L
+    }
+  }
+}
+
+cat_solution(36L, answer1 - neighbor_count)
