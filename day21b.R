70ff90fedb9bf0eed9e6cfee5cf424e607a12ae0
Bob Jansen <bobjansen@gmail.com>
2022-12-25 09:52:12 +0100

    Implement part 2 of day 21

--- day21b.R
+++ day21b.R
@@ -1,7 +1,5 @@
 source("helpers.R")
 
-options(digits = 18L) # For printing large numerics without scientific notation
-
 txt <- "root: pppw + sjmn
 dbpl: 5
 cczh: sllz + lgvd
@@ -19,7 +17,7 @@ drzm: hmdt - zczc
 hmdt: 32"
 txt <- readFile("input21.txt")
 
-part2 <- FALSE
+part2 <- TRUE
 
 monkies <- data.table::fread(text = txt, sep = ":", header = FALSE)
 setnames(monkies, c("monkey", "value"))
@@ -31,27 +29,52 @@ monkies[, ':='(
 )]
 monkies <- setorder(monkies, "monkey")
 
-last_rows <- Inf
-while (nrow(monkies) < last_rows && nrow(monkies) > 1L) {
-  last_rows <- nrow(monkies)
-  monkies[
-    monkies[resolved == TRUE],
-    value1 := as.numeric(i.value),
-    on = .(input1 = monkey)]
-  monkies[
-    monkies[resolved == TRUE],
-    value2 := as.numeric(i.value),
-    on = .(input2 = monkey)]
-  monkies <- monkies[resolved == FALSE]
+resolve <- function(monkies) {
+  last_rows <- Inf
 
-  todo <- monkies[, !is.na(value1) & !is.na(value2)]
-  monkies[todo == TRUE, resolved := TRUE]
-  monkies[todo & op == "-", value := value1 - value2]
-  monkies[todo & op == "+", value := value1 + value2]
-  monkies[todo & op == "*", value := value1 * value2]
-  monkies[todo & op == "/", value := value1 / value2]
+  while (nrow(monkies) < last_rows && nrow(monkies) > 1L) {
+    last_rows <- nrow(monkies)
+    monkies[
+      monkies[resolved == TRUE],
+      value1 := as.numeric(i.value),
+      on = .(input1 = monkey)]
+    monkies[
+      monkies[resolved == TRUE],
+      value2 := as.numeric(i.value),
+      on = .(input2 = monkey)]
+    monkies <- monkies[resolved == FALSE]
+
+    todo <- monkies[, !is.na(value1) & !is.na(value2)]
+    monkies[todo == TRUE, resolved := TRUE]
+    monkies[todo & op == "-", value := value1 - value2]
+    monkies[todo & op == "+", value := value1 + value2]
+    monkies[todo & op == "*", value := value1 * value2]
+    monkies[todo & op == "/", value := value1 / value2]
+  }
+  monkies
 }
 
-cat_solution(41L, monkies[, value])
+# For printing large numericals without scientific notation set digits to high
+options(digits = 18L)
+
+if (!part2) {
+  monkies <- resolve(monkies)
+  cat_solution(41L, monkies[, value])
+} else {
+  monkies <- monkies[monkey != "humn"]
+
+  for (i in c(3555057453229)) {
+    test_set <- resolve(
+      rbind(
+        copy(monkies),
+        list(monkey = "humn", value = i, resolved = TRUE),
+        fill = TRUE))
+    print(test_set)
+    if (test_set[, value1 == value2]) {
+      break
+    }
+  }
+  cat_solution(42L, i)
+}
 
 options(digits = 8L)
